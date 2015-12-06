f93453b7d9a6482b4d25901cae20ee6fee817b2e
gncurran <gnc115@gmail.com>
2015-12-06 00:05:27 -0500

    Done.

--- lib/project_euler_collatz.rb
+++ lib/project_euler_collatz.rb
@@ -1,14 +1,30 @@
 def even_next(n)
+  n = n/2
 end
 
 def odd_next(n)
+  n = (3*n + 1)
 end
 
 def next_value(n)
+  n.even? ? even_next(n) : odd_next(n)
 end
 
 def collatz(n)
+  array = [n]
+  while n > 1             #add 'do'?
+    n = next_value(n)
+    array << n
+  end
+  return array
 end
 
 def longest_collatz
+  i = 0
+  find_longest = 0
+  999999.downto(1) do |n|
+    count = collatz(n).length
+    i, find_longest = count, n if count > i
+  end
+  return find_longest
 end
